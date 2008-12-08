9df542c154008dc537f010082d8134ca43994c08
Frederic Cormier <fdco@mac.com>
2008-12-08 15:17:45 +0100

    added some test for tones

--- lib/tones.rb
+++ lib/tones.rb
@@ -366,18 +366,3 @@ module Tones
 	end #of Scale
 end
 
-#include Tones
-
-#puts c = Chord.new( "c", 1, "MAJ")
-#puts c.invert!(SECOND)
-# c.invert!(FITH)
-# puts c
-
-# puts s = Tones::Scale.new( "F#",2, "MIXOLYDIAN")
-# puts c13 = Tones::Chord.new( 'A',1,"MAJ13")
-# puts c13.name
-# c13.invert! FITH
-# puts c13
-
-# puts
-# puts cmaj = Tones::Chord.new('c', 1, 'MAJ', Tones::SECOND)			# => G1 C2 E2
