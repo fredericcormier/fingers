5cbf5c323a7dcd3e324b588195e905c3ba8b3b9d
Frederic Cormier <fdco@mac.com>
2010-03-16 15:07:59 +0100

    Added preliminary NoteArray

--- notearray.rb
+++ notearray.rb
@@ -1,11 +1,29 @@
-class WrapArray < Array
-  def [] index
-    super(index % self.length)
+
+
+
+class NoteArray
+  class WrapArray < Array
+    def [] index
+      super(index % self.length)
+    end
+  end  
+def initialize
+    @wa = WrapArray.new %w[A A# B C C# D D# E F F# G G#]
+    def [] index
+      @wa[index]
+    end
   end
+  
 end
 
 
 
-Notes =  %w[A A# B C C# D D# E F F# G G#]
-w = WrapArray.new(12) {|i| Notes[i]}
-36.times { |i| puts w[i]  }
+
+
+n = NoteArray.new
+puts n[-11]
+
+puts n[11]
+
+puts n[57]
+
